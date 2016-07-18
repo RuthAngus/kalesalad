624eec3004e118106c34bf3447722c388ca453ba
RuthAngus <ruth.angus@astro.ox.ac.uk>
2016-07-18 10:08:22 +0000

    adding download epic

---
+++ code/download_epic.py
@@ -0,0 +1,34 @@
+from __future__ import division, print_function
+import os
+import requests
+import pandas as pd
+from StringIO import StringIO
+import numpy as np
+
+def get_catalog(name, basepath="data"):
+    fn = os.path.join(basepath, "{0}.h5".format(name))
+    if os.path.exists(fn):
+        return pd.read_hdf(fn, name)
+    csvfn = os.path.join(basepath, "{0}.csv".format(name))
+    if not os.path.exists(csvfn):
+        if not os.path.exists(basepath):
+            os.makedirs(basepath)
+        print("Downloading {0}...".format(name))
+        url = ("http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/"
+               "nph-nstedAPI?table={0}&select=*").format(name)
+        r = requests.get(url)
+        if r.status_code != requests.codes.ok:
+            r.raise_for_status()
+        with open(csvfn, "w") as f:
+            f.write(r.content)
+    df = pd.read_csv(csvfn, low_memory=False)
+    df.to_hdf(fn, name, format="t")
+    return df
+
+if __name__ == "__main__":
+    df = get_catalog("k2targets")
+
+    ids = np.sort(df.epic_number)
+    inds = np.argsort(df.epic_number)
+    print(inds[0])
+    idlist = ids[1951:3626]
