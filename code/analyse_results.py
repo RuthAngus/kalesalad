ec06b423d2eda3df8d295a51b5eb08b13abe0c1e
RuthAngus <ruth.angus@astro.ox.ac.uk>
2018-01-02 09:40:19 +0000

    plotting 10% margins

--- code/analyse_results.py
+++ code/analyse_results.py
@@ -9,15 +9,23 @@ import pandas as pd
 date = "2018-01-01"
 df = pd.read_csv("c1_periods_{}.txt".format(date), delimiter=" ")
 
+xs = np.linspace(min(df.ACF_period), max(df.pgram_period), 100)
 plt.clf()
 plt.plot(df.ACF_period, df.pgram_period, "k.")
+plt.plot(xs, xs, "--", color=".5")
+plt.plot(xs, xs*1.1, "--", color="r")
+plt.plot(xs, xs*.9, "--", color="r")
 plt.xlabel("ACF period")
 plt.ylabel("Pgram period")
-plt.savefig("ACF_vs_pgram")
 
 percent = 10
 lower = df.ACF_period.values - df.ACF_period.values/percent
 upper = df.ACF_period.values + df.ACF_period.values/percent
-success_inds = (lower < df.pgram_period.values) | (df.pgram_period.values < upper)
+success_inds = (lower < df.pgram_period.values) * \
+    (df.pgram_period.values < upper)
 
-print(len(df.ACF_period), sum(success_inds))
+plt.plot(df.ACF_period[success_inds], df.pgram_period[success_inds], "b.")
+plt.savefig("ACF_vs_pgram")
+
+print(df.epic_id.values[success_inds])
+print(sum(success_inds), "successful out of", len(df.ACF_period))
