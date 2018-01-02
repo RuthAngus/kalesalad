85d06244c2c97c2491fa09ddad4e3a2123f91764
RuthAngus <ruth.angus@astro.ox.ac.uk>
2018-01-02 09:15:16 +0000

    running acf and pgram and saving incremental progress

--- code/kalesalad.py
+++ code/kalesalad.py
@@ -1,7 +1,6 @@
 # Uses acf method to measure rotation periods for downloaded everest light
 # curves.
 
-from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
 import pyfits
@@ -14,6 +13,7 @@ import pandas as pd
 import glob
 import scipy.stats as sps
 import rotation as ro
+import datetime
 
 plotpar = {'axes.labelsize': 20,
            'text.fontsize': 20,
@@ -47,15 +47,31 @@ def process_data(file):
     return x[m], y[m], yerr[m]
 
 
-def run_acf(c, epic, x, y, plot=False):
+def run_acf(c, epic, plot=True):
     """
     Run the ACF on a light curve in the specified campaign.
+    FOR PARALLEL RUNS.
     c (str): campaign, e.g. "c01".
     fn (str): fits file name for a target in campaign c.
     """
 
     #period, acf_smooth, lags, rvar, peaks, dips, leftdips, rightdips, \
     #bigpeaks = simple_acf(x, y)
+
+    v = "2.0"
+    filen = "hlsp_everest_k2_llc_{0}-c{1}_kepler_v{2}_lc.fits"\
+        .format(epic, c.zfill(2), v)
+    file = "data/c{0}/{1}".format(c.zfill(2), filen)
+
+    # Load time and flux
+    if not os.path.exists(file):
+        print(file, "file not found")
+        return None
+    try:
+        x, y, yerr = process_data(file)
+    except (IOError, ValueError):
+        print("Bad file", file)
+        return None
 
     # compute the acf
     period, acf_smooth, lags, rvar, peaks = simple_acf(x, y)
@@ -77,6 +93,10 @@ def run_acf(c, epic, x, y, plot=False):
         plt.subplots_adjust(left=.16, bottom=.12, hspace=.4)
         plt.savefig("acfs/{}_acf".format(epic))
 
+    # Measure LS period
+    star = ro.prot(kepid=epic, x=x, y=y, yerr=yerr)
+    pgram_period = star.pgram_ps(plot=True)
+
     return epic, period
 
 
@@ -84,18 +104,21 @@ def run_kalesalad(N):
     """
     Measure all rotation periods in a campaign - non parallel (for tests).
     """
+    todays_date = datetime.date.today()
+    results_file = "c{0}_periods_{1}.txt".format(c, todays_date)
+    f.write("{0} {1} {2} {3}\n".format("epic_id", "ACF_period",
+                                       "pgram_period", "pgram_period_err")
+
     c = str(sys.argv[1])
     df = pd.read_csv("c{}_targets.txt".format(c.zfill(2)), dtype=str)
     fns = df["epid"].values
-    # periods, epics = [np.zeros(N) for i in range(2)]
+    acf_periods, pgram_periods, pgram_period_errs, epics = [np.zeros(N) for i
+                                                            in range(4)]
     for i, epic in enumerate(fns[:N]):
-
         v = "2.0"
         filen = "hlsp_everest_k2_llc_{0}-c{1}_kepler_v{2}_lc.fits"\
             .format(epic, c.zfill(2), v)
-        print(filen)
         file = "data/c{0}/{1}".format(c.zfill(2), filen)
-        print(file)
 
         # Load time and flux
         if not os.path.exists(file):
@@ -108,28 +131,28 @@ def run_kalesalad(N):
             return None
 
         # Measure ACF period
-        print(epic)
-        _, acf_period = run_acf(c, epic, x, y, plot=True)
+        _, acf_period = run_acf(c, epic, plot=True)
 
         # Measure LS period
         star = ro.prot(kepid=epic, x=x, y=y, yerr=yerr)
         pgram_period = star.pgram_ps(plot=True)
-        print(pgram_period)
 
-        # periods[i] = period
-        # epics[i] = epic
-    # res = dict({"epic": np.array(epics), "period": np.array(periods)})
-    # res = pd.DataFrame(res)
-    # res.to_csv("results.")
+        with open(results_file, "a") as f:
+            f.write("{0} {1} {2} {3}\n".format(epic, acf_period,
+                                               pgram_period[0],
+                                               pgram_period[1]))
 
 if __name__ == "__main__":
     from functools import partial
     c = str(sys.argv[1])
 
-    open("c{0}_periods.txt".format(c), "w")
+    # open("c{0}_periods.txt".format(c), "w")
 
-    run_kalesalad(2)
+    run_kalesalad(100)
     assert 0
+
+    df = pd.read_csv("c{}_targets.txt".format(c.zfill(2)), dtype=str)
+    fns = df["epid"].values
 
     f = partial(run_acf, c)
 
@@ -137,7 +160,7 @@ if __name__ == "__main__":
     for val in pool.map(f, fns):
         if val is None:
             continue
-        epic, period = val
+        epic, acf_period, epic_period = val
         # append data to file
         with open("c{0}_periods.txt".format(c), "a") as f:
             f.write("{0} {1} \n".format(epic, period))
